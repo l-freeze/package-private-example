d8a60fbb3a6bb82a27b458180527cd3685c7d87e
l-freeze <mmmm.yamamoto@gmail.com>
2023-04-15 12:43:10 +0900

    Change: Constructor property attribute example

--- src/SpaceA/Callee.php
+++ src/SpaceA/Callee.php
@@ -7,6 +7,7 @@ use LFreeze\PackagePrivate\PackagePrivateAttribute;
 
 final class Callee {
     use PackagePrivateAttribute;
+    
 
     #[PackagePrivate]
     private int $packagePrivateInt = 9876543210;
@@ -14,7 +15,7 @@ final class Callee {
     #[PackagePrivate]
     private string $packagePrivateString = "DefaultPrivateString";
 
-    public function __construct(public $namedArguments1 = 'DefaultPropertyX', public $namedArguments2 = 'DefaultPropertyY') {
+    public function __construct(private $namedArguments1 = 'DefaultPropertyX', #[PackagePrivate] private $namedArguments2 = 'DefaultPropertyY') {
     }
 
     #[PackagePrivate]
--- src/SpaceB/Caller.php
+++ src/SpaceB/Caller.php
@@ -9,11 +9,14 @@ readonly final class Caller {
     }
 
     public function do() {
+        echo '['.__METHOD__.']'.PHP_EOL;
         Callee::assignCallerNamespaceName(__NAMESPACE__);
 
         $example = (new Callee(namedArguments2: "partialarguments"))->__create();
+        echo '$example->namedArguments1: '. $example->namedArguments1.PHP_EOL;
+        echo '$example->namedArguments2: '. $example->namedArguments2.PHP_EOL;
         $example->packagePrivateInt = 13;
-        echo '$example->packagePrivateInt:'. $example->packagePrivateInt.PHP_EOL;
+        echo '$example->packagePrivateInt: '. $example->packagePrivateInt.PHP_EOL;
         echo $example->packagePrivateMethod(param:" AssignParam1 ", param2: " AssignParam2 ").PHP_EOL;
         echo $example->packagePrivateMethod(param2: " AssignParam2 ", param:" AssignParam1 ").PHP_EOL;
     }
